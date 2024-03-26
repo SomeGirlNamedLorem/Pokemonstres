24511ff670af4bb50f354530818f0238949e630b
Timothe-Picot <154351842+Timothe-Picot@users.noreply.github.com>
2024-03-26 10:54:31 +0100

    et rebelotte

--- data/class_attack.js
+++ data/class_attack.js
@@ -1,11 +1,12 @@
 class Attack {
     static all_moves={};
-    constructor(attackData) {
+    constructor(attackData,speed) {
         this.moveId = attackData.move_id;
         this.name = attackData.name;
         this.type = attackData.type;
         this.power = attackData.power;
         this.duration = attackData.duration;
+        this.speed = speed;
         Attack.all_moves[this.moveId]=this;
     }
 
